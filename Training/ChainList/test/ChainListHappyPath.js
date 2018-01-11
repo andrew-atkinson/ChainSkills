df5d829014cbc2dc30c9c94fd86ee52b1ca2a930
andrew-atkinson <blind.sam@gmail.com>
2018-01-11 17:36:10 -0500

    adds: basic test for selling contract

---
+++ Training/ChainList/test/ChainListHappyPath.js
@@ -0,0 +1,46 @@
+var ChainList = artifacts.require('./ChainList.sol')
+
+contract('ChainList', function (accounts) {
+
+  var ChainListInstance;
+  var seller = accounts[1];
+  var articleName = "article 1";
+  var articlePrice = 10;
+  var articleDescription = "this is an article description"
+
+  it('should be initialized with empty values', function () {
+    return ChainList
+      .deployed()
+      .then(function (instance) {
+        return instance
+          .getArticle
+          .call();
+      })
+      .then(function (data) {
+        assert.equal(data[0], 0x0, 'seller must be empty');
+        assert.equal(data[1], '', 'article name must be empty');
+        assert.equal(data[2], '', 'description must be empty');
+        assert.equal(+ data[3], 0, 'article price must be zero');
+      })
+  })
+
+  it('should sell an article', function () {
+    return ChainList
+      .deployed()
+      .then(function (instance) {
+        ChainListInstance = instance;
+        return ChainListInstance.sellArticle(articleName, articleDescription, web3.toWei(articlePrice, "ether"), {from: seller})
+      })
+      .then(function () {
+        return ChainListInstance
+          .getArticle
+          .call()
+      })
+      .then(function (data) {
+        assert.equal(data[0], seller, "seller must be " + seller);
+        assert.equal(data[1], articleName, "article name must be " + articleName);
+        assert.equal(data[2], articleDescription, "article description must be " + articleDescription);
+        assert.equal(+ data[3], web3.toWei(articlePrice, "ether"), "Price must be " + web3.toWei(articlePrice, "ether"))
+      })
+  })
+})
