7d3bea0a7650a2620e2b9db2e332e6af9fe24726
andrew-atkinson <blind.sam@gmail.com>
2018-01-14 15:55:36 -0500

    updates: contract now allows multiple articles

--- Training/ChainList/contracts/ChainList.sol
+++ Training/ChainList/contracts/ChainList.sol
@@ -1,44 +1,82 @@
 pragma solidity ^0.4.11;
 
+
 contract ChainList {
-    // State variables
-    address seller;
-    address buyer;
-    string name;
-    string description;
-    uint256 price;
+    // Custom Types
+    struct Article {
+        uint id;
+        address seller;
+        address buyer;
+        string name;
+        string description;
+        uint256 price;
+    }
+
+    //state
+    mapping (uint => Article) public articles;
+    uint public articleCounter;
 
     // events
-    event SellArticleEvent(address indexed _seller, string _name, uint256 _price);
-    event BuyArticleEvent(address indexed _seller, address indexed _buyer, string _name, uint256 _price);
+    event SellArticleEvent(uint indexed _id, address indexed _seller, string _name, uint256 _price);
+    event BuyArticleEvent(uint indexed _id, address indexed _seller, address indexed _buyer, string _name, uint256 _price);
 
     // sell an article
     function sellArticle(string _name, string _description, uint256 _price) public {
-        seller = msg.sender;
-        name = _name;
-        description = _description;
-        price = _price;
-        SellArticleEvent(seller, name, price);
+        articleCounter++;
+
+        articles[articleCounter] = Article(
+            articleCounter,
+            msg.sender,
+            0x0,
+            _name,
+            _description,
+            _price
+        );
+
+        SellArticleEvent(articleCounter, msg.seller, _name, _price);
     }
 
     // buy an article
-    function buyArticle() public payable {
-        require(seller != 0x0); //is there an article for sale?
-        require(buyer == 0x0); //is the article not already sold?
-        require(msg.sender != seller); //seller can't buy own article
-        require(msg.value == price); //is msg.value the same as the listed price
-        buyer = msg.sender; // assign the sender as the buyer
-        seller.transfer(msg.value); // buyer buys article
-        BuyArticleEvent(seller, buyer, name, price); //trigger notifications
+    function buyArticle(uint _id) public payable {
+        require(articleCounter > 0);
+        require(_id > 0 && _id <= articleCounter);
+        // store the article
+        Article storage article = articles[_id];
+
+        require(article.buyer == 0x0); //is the article not already sold?
+        require(article.seller != msg.sender); //seller can't buy own article
+        require(article.price == msg.value); //is msg.value the same as the listed price
+        article.buyer = msg.sender; // assign the sender as the buyer
+        article.seller.transfer(msg.value); // buyer buys article
+        BuyArticleEvent(_id, article.seller, article.buyer, article.name, article.price); //trigger notifications
     }
 
-    // get the article
-    function getArticle() public constant returns (
-        address _seller,
-        address _buyer,
-        string _name,
-        string _description,
-        uint256 _price) {
-        return(seller, buyer, name, description, price);
+    // get # of articles
+    function getNumberOfArticles() public constant returns (uint) {
+        return articleCounter;
     }
+
+    // returns article IDs for sale
+    function getArticlesForSale() public constant returns (uint[]){
+        require(articleCounter > 0);
+
+        uint[] memory articleIds = new uint[](articleCounter);
+        uint numberOfArticlesForSale = 0;
+
+        for (uint i = 1; i <= articleCounter; i++) {
+            if (articles[i].buyer == 0x0) {
+                articleIds[numberOfArticlesForSale] = articles[i].id;
+                numberOfArticlesForSale++;
+            }
+        }
+
+        uint[] memory forSale = new uint[](numberOfArticlesForSale);
+        for (uint j = 0; j < numberOfArticlesForSale; j++){
+            forSale[j] = articleIds[j];
+        }
+
+        return (forSale);
+    }
+
+    
 }
