274160d4109ff7d89664e0037aec1f4e163b7c20
andrew-atkinson <blind.sam@gmail.com>
2018-01-21 15:46:31 -0500

    adds: frontend files to github pages

--- Training/ChainList/deployfrontend.sh
+++ Training/ChainList/deployfrontend.sh
@@ -1,5 +1,5 @@
-rsync -r src/ ../../docs/
-rsync build/contracts/ChainList.json ../../docs/
+rsync -r src/ ../docs/
+rsync build/contracts/ChainList.json ../docs/
 git add . 
 git commit -m "adds: frontend files to github pages"
 git push
